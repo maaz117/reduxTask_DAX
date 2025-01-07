ce7bace502433036e7b640262bb9b38aebd69847
maaz117 <maazadnan3@gmail.com>
2025-01-07 16:59:57 +0500

    added error page

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,29 @@ import Home from './components/Home'
 import Login from './components/login'
 import About from './components/about'
 import User from './components/user'
+import Error from './components/error'
 
 function App() {
     const router = createBrowserRouter([
       {
         path: '/',
         element: <><Navbar /><Home /></>,
+        errorElement: <Error />,
       },
       {
         path: '/login',
         element: <><Navbar /><Login /></>,
+        errorElement: <Error />,
       },
       {
         path: '/about',
         element: <><Navbar /><About /></>,
+        errorElement: <Error />,
       },
       {
         path: '/user/:username',
         element: <><Navbar /><User /></>,
+        errorElement: <Error />,
       }
   ])
 
---
+++ src/components/error.jsx
@@ -0,0 +1,7 @@
+function Error() {
+    return (
+        <div>404 not found</div>
+    )
+};
+
+export default Error;
